dad55bc669a6e1d33ea090bf70f75eddd8d8cf25
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-04-01 23:15:41 +0530

    Add files via upload

---
+++ Musicart/backend/models/Feedback.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const feedbackSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  feedbackType: {
+    type: String,
+    enum: ['bugs', 'feedback', 'query'],
+    required: true
+  },
+  feedbackText: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+module.exports = Feedback;
