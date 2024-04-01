103ef540eba1b85c616b34b0554a3d6afc79f7f5
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-04-01 23:34:57 +0530

    Update CartPage.js

--- Musicart/frontend/src/components/CartPage/CartPage.js
+++ Musicart/frontend/src/components/CartPage/CartPage.js
@@ -11,7 +11,7 @@ const CartPage = () => {
   const fetchCartItems = async () => {
     try {
       const token = localStorage.getItem('token');
-      const response = await fetch('http://localhost:3000/cart', {
+      const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/cart', {
         headers: {
           Authorization: token,
         },
