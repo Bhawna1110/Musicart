d986cff2b64152d63aa91075c4565df634ad075c
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-04-05 00:00:35 +0530

    Update Dashboard.js

--- Musicart/frontend/src/components/Dashboard/Dashboard.js
+++ Musicart/frontend/src/components/Dashboard/Dashboard.js
@@ -151,7 +151,7 @@ const Dashboard = () => {
 
   const handleLogin = () => {
     localStorage.removeItem('token');
-    navigate('/');
+    navigate('/login');
   };
 
   const handleSignup = () => {
