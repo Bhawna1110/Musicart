4b3c84da60c7ed97190b04c0a85ca3c260707465
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-04-09 20:34:22 +0530

    Update MyInvoice.js

--- Musicart/frontend/src/components/MyInvoice/MyInvoice.js
+++ Musicart/frontend/src/components/MyInvoice/MyInvoice.js
@@ -42,7 +42,7 @@ const InvoicesPage = () => {
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/orders');
+        const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-uyx4.onrender.com/orders');
         if (!response.ok) {
           throw new Error('Failed to fetch orders');
         }
@@ -64,7 +64,7 @@ const InvoicesPage = () => {
       if (token) {
         setIsLoggedIn(true);
       }
-      const response = await fetch('http://localhost:3000/user-data', {
+      const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-uyx4.onrender.com/user-data', {
         headers: {
           Authorization: token,
         },
