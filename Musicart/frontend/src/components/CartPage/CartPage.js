ac25e771926549e86cde7fa7f0dd8795c241150d
Bhawna Suman <63039132+Bhawna1110@users.noreply.github.com>
2024-04-10 10:39:02 +0530

    Update CartPage.js

--- Musicart/frontend/src/components/CartPage/CartPage.js
+++ Musicart/frontend/src/components/CartPage/CartPage.js
@@ -25,8 +25,7 @@ const CartPage = () => {
   const fetchCartItems = async () => {
     try {
       const token = localStorage.getItem('token');
-      // const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/cart', {
-      const response = await fetch('http://localhost:3000/cart', {
+      const response = await fetch('https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/cart', {
 
         headers: {
           Authorization: token,
@@ -76,7 +75,7 @@ const CartPage = () => {
   };
 
   const handlePlaceOrder = () => {
-    navigate('/checkoutpage', { state: { orderTotal } }); // need to check on this.
+    navigate('/checkoutpage', { state: { orderTotal } });
   };
 
 
@@ -92,8 +91,8 @@ const CartPage = () => {
     try {
       const newQuantity = parseInt(e.target.value);
       const token = localStorage.getItem('token');
-      // const response = await fetch(`https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/update-quantity/${itemId}`, {
-      const response = await fetch(`http://localhost:3000/update-quantity/${itemId}`, {
+    const response = await fetch(`https://sumanbhawna11-gmail-com-cuvette-final-66kf.onrender.com/update-quantity/${itemId}`, {
+     
 
         method: 'PUT',
         headers: {
